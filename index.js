0750ac5289b8496c59a2e9f5b06919e633422cc6
Lucas <lucastecher97421fabien@gmail.com>
2022-06-16 19:58:01 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+    loader.classList.add("fondu-out");
+})
+
+const audio = new Audio("./music.mp3");
+const btn = document.querySelector("#play");
+const imageBtn = document.querySelector("#playImg");
+
+let clickStatus = false;
+btn.addEventListener("click", () => {
+    if (!clickStatus) {
+        imageBtn.src = "./pause.png";
+        audio.play();
+        clickStatus = true;
+    } else if (clickStatus) {
+        imageBtn.src = "./play.png";
+        audio.pause();
+        clickStatus = false;
+    }
+})
